101f1b144985e5fc94f0680adee9255ae3f6d241
misha <medinskijmisa228@gmail.com>
2025-05-13 16:57:36 +0300

    included the litext_text app in urls.py configured it for celery

--- LiText_backend/litext_backend/litext_text/views.py
+++ LiText_backend/litext_backend/litext_text/views.py
@@ -1,3 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.request import Request
+from rest_framework.response import Response
