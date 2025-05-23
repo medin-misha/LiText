fc71167bcc038c87d7f0e83d28fa9821545db4e5
misha <medinskijmisa228@gmail.com>
2025-05-23 10:34:37 +0300

    filled tasks

--- LiText_backend/litext_backend/litext_text/utils.py
+++ LiText_backend/litext_backend/litext_text/utils.py
@@ -22,21 +22,3 @@ def save_text(text: str, user_id: int) -> dict:
         return response.json()
     raise BadRequest(response.json())
 
-
-@shared_task
-def update_text(new_body: str, text: str) -> None:
-    update_text_data: dict = {
-        "body": new_body
-    }
-    response = requests.patch(url=settings.TEXT_SAVER_URL + "/api/texts/" + f"{text}", json=update_text_data)
-    if 200 <= response.status_code <= 300:
-        return
-    raise Http404(response.json())
-
-
-@shared_task
-def delete_text(id: str) -> None:
-    response = requests.delete(url=settings.TEXT_SAVER_URL + "/api/texts/" + f"{id}")
-    if 200 <= response.status_code <= 300:
-        return
-    raise Http404(response.json())
